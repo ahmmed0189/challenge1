7385889920afd1b962ceb08c56bf0c845bc9bcca
dein Github username <deineGithubEMail@mail.de>
2024-08-21 13:47:51 +0200

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 30),
+                  Image(
+                    image: AssetImage('assets/ah.jpg'),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'SUCCESSFUL ADDED',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 123, 135, 150),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Wallet Balance: \$2200.00',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 123, 135, 150),
+                    ),
+                  ),
+                  Text(
+                    'Total Balance: \$5200.00',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 123, 135, 150),
+                    ),
+                  ),
+                  SizedBox(height: 90),
+                  Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'AGAIN',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 123, 135, 150),
+                          ),
+                        ),
+                        Text(
+                          'DONE',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 123, 135, 150),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
